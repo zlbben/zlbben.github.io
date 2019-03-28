91c7859ca94bc1ec1950da0de23a90249bdb6879
liben.zlb <liben.zlb@alibaba-inc.com>
2019-03-28 20:25:49 +0800

    123

--- projects/check_book/src/selector/piaotian.ts
+++ projects/check_book/src/selector/piaotian.ts
@@ -1,8 +1,8 @@
 export default {
-  SEARCH_INPUT: "#searchkey",
+  SEARCH_INPUT: '#searchkey',
   SEARCH_BUTTON:
-    "body > div.main.nav > div.main.m_top > div > form > input.button",
+    'body > div.main.nav > div.main.m_top > div > form > input.button',
   NEW_TEXT:
-    "#content > table > tbody > tr:nth-child(4) > td > table > tbody > tr > td:nth-child(2) > div > a",
-  BIQUGE: "#info > p:nth-child(5) > a"
+    '#content > table > tbody > tr:nth-child(4) > td > table > tbody > tr > td:nth-child(2) > div > a',
+  BIQUGE: '#info > p:nth-child(5) > a'
 };
