b59c9eb74649ed10b080d364f3181292ea885eb3
liben.zlb <liben.zlb@alibaba-inc.com>
2019-03-28 20:27:47 +0800

    123

--- projects/check_book/out/selector/piaotian.js
+++ projects/check_book/out/selector/piaotian.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    SEARCH_INPUT: "#searchkey",
-    SEARCH_BUTTON: "body > div.main.nav > div.main.m_top > div > form > input.button",
-    NEW_TEXT: "#content > table > tbody > tr:nth-child(4) > td > table > tbody > tr > td:nth-child(2) > div > a",
-    BIQUGE: "#info > p:nth-child(5) > a"
+    SEARCH_INPUT: '#searchkey',
+    SEARCH_BUTTON: 'body > div.main.nav > div.main.m_top > div > form > input.button',
+    NEW_TEXT: '#content > table > tbody > tr:nth-child(4) > td > table > tbody > tr > td:nth-child(2) > div > a',
+    BIQUGE: '#info > p:nth-child(5) > a'
 };
